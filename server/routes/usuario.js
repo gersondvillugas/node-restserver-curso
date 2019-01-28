d5c3e2d0172fb30e0960582056649777ed494ab8
gerson <gerson.villugas.a@uni.pe>
2019-01-28 14:28:48 -0500

    makeangry

---
+++ server/routes/usuario.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+app.get('/usuario', function (req, res) {
+    res.json('get Usuario Loca')
+  });
+
+  app.post('/usuario', function (req, res) {
+      let body=req.body;
+    res.json({
+      persona:body
+    });
+  });
+  app.put('/usuario/:id', function (req, res) {
+   let id=req.param.id;
+    res.json({
+       id:id
+    });
+  });
+  
+  app.delete('/usuario', function (req, res) {
+    res.json('delete Usuario')
+  });
+  module.exports=app;
--- server/server.js
+++ server/server.js
@@ -2,32 +2,16 @@ const express = require('express')
 const app = express()
 require('./config/config')
 const bodyParser=require('body-parser')
-
+const mongoose=require('mongoose');
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
+app.use(require('./routes/usuario'));
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/usuario', function (req, res) {
-    res.json('get Usuario')
-  });
 
-  app.post('/usuario', function (req, res) {
-      let body=req.body;
-    res.json({
-      persona:body
-    });
-  });
-  app.put('/usuario/:id', function (req, res) {
-   let id=req.param.id;
-    res.json({
-       id
-    });
-  });
-  
-  app.delete('/usuario', function (req, res) {
-    res.json('delete Usuario')
-  });
-  
-   
+   mongoose.connect('mongodb://localhost:27017/cafe',(err,res)=>{
+       if(err) throw err;
+       console.log('base de datos online');
+   });
 app.listen(process.env.PORT,()=> {
 console.log('escuchando puerto:',process.env.PORT);
 });
